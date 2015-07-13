49aa37c9874db2f22877f249d6a804a17be23968
Matt <matt.antalek@yahoo.com>
2015-07-13 17:05:56 -0500

    updated license info in files

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ def readme():
     return text
 
 setup(name='pydendroheatmap',
-      version='1.0.1',
+      version='1.0.2',
       description='Tool for creating heatmaps, where rows and columns are organized by hierarchical clusters'
                   'as seen in http://code.activestate.com/recipes/578175-hierarchical-clustering-heatmap-python/',
       url='https://github.com/themantalope/pydendroheatmap',
-      author='Matt Antalek',
+      author='Matt Antalek Jr',
       author_email='matthew.antalek@northwestern.edu',
       license='MIT',
       packages=['pydendroheatmap'],
