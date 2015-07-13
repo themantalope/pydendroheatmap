668272196459d20797152eb4c4ed800eda5f7bea
Matt <matt.antalek@yahoo.com>
2015-07-13 17:13:02 -0500

    made a couple changes to setup, fixed spelling errors etc

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ def readme():
     return text
 
 setup(name='pydendroheatmap',
-      version='1.0.2',
+      version='1.0.3',
       description='Tool for creating heatmaps, where rows and columns are organized by hierarchical clusters'
-                  'as seen in http://code.activestate.com/recipes/578175-hierarchical-clustering-heatmap-python/',
+                  ' as seen in http://code.activestate.com/recipes/578175-hierarchical-clustering-heatmap-python/',
       url='https://github.com/themantalope/pydendroheatmap',
       author='Matt Antalek Jr',
       author_email='matthew.antalek@northwestern.edu',
