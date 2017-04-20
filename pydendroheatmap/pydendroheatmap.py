d963a09d915665a54726af7d7c2daf4eb5eb873c
Sam De Meyer <sam.demeyer93@gmail.com>
2017-04-20 12:27:15 +0200

    fix row label alingment issue

--- pydendroheatmap/pydendroheatmap.py
+++ pydendroheatmap/pydendroheatmap.py
@@ -200,7 +200,7 @@ class DendroHeatMap(object):
             for i in range(0, self.heat_map_rows):
                 if(self.row_labels):
                     if(len(self.row_labels) < self.max_row_labels):
-                        self.heat_map_axes.text(self.heat_map_cols-0.5, i-2, ' '+self.row_labels[i], size=self.row_labels_size)
+                        self.heat_map_axes.text(self.heat_map_cols-0.5, i-1/2, ' '+self.row_labels[i], size=self.row_labels_size)
 
             for i in range(0, self.heat_map_cols):
                 if(self.col_labels):
